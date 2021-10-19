4495e12bf73e5c54bb28e7fa3c4e962fde9a0e1f
Arttu Höylä <33629266+ajh657@users.noreply.github.com>
2021-10-19 11:00:39 +0300

    Done

---
+++ ClosestDivisibleByFinder/ClosestDivisibleByFinder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClosestDivisibleByFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                int dividingNumber = int.Parse(args[0]);
+                int divisibleByNumber = int.Parse(args[1]);
+
+                for (int i = dividingNumber; i > 0; i--)
+                {
+                    if (i % divisibleByNumber == 0)
+                    {
+                        Console.WriteLine($"{i} is divisible by {divisibleByNumber}");
+                    }
+                }
+            }
+        }
+    }
+}
